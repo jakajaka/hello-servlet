f1f73f0997fb3e79739afa37594cb5d6d9121442
priprave15 <jaka.sporin@gmail.com>
2012-07-09 16:28:41 +0200

    nas prvi servlet

--- src/main/java/org/openblend/hello/HelloWorld.java
+++ src/main/java/org/openblend/hello/HelloWorld.java
@@ -1,7 +1,20 @@
 package org.openblend.hello;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * @author <a href="mailto:tomaz.cerar@redhat.com">Tomaz Cerar</a>
  */
-public class HelloWorld {
+@WebServlet(name="HelloWorldServlet", urlPatterns = {"/hello"})
+public class HelloWorld extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name =req.getParameter("name");
+       resp.getWriter().println("Hello World" + name);    //To change body of overridden methods use File | Settings | File Templates.
+    }
 }
